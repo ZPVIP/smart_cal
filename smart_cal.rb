94f6578bbc7686df0fdab1368c10b593056382a6
Hake Huang <hake.huang@nxp.com>
2016-12-01 21:20:35 +0800

    add bits
    Signed-off-by: Hake Huang <hake.huang@nxp.com>

--- smart_cal.rb
+++ smart_cal.rb
@@ -1,4 +1,6 @@
 
+#require 'green_shoes'
+
 class String
   def from_hex
     self.to_i(16)
@@ -18,10 +20,10 @@ class Fixnum
         self.to_s(16) 
     end
     def to_bin
-        self.to_s(2) 
+        self.to_s(2)
     end
     def to_dec
-        self.to_s 
+        self.to_s
     end
 end
 
@@ -40,7 +42,25 @@ class Smart_Cal
   end
 
   def to_s
-    @number.to_s
+    case @mode
+      when OperationType::DEC
+        @number.to_dec
+      when OperationType::HEX
+        @number.to_hex
+      when OperationType::BIN
+        @number.to_bin
+    end
+  end
+
+  def mode
+    case @mode
+      when OperationType::DEC
+        ""
+      when OperationType::HEX
+        "0x"
+      when OperationType::BIN
+        "0B"
+    end
   end
 
   
@@ -60,26 +80,21 @@ class Smart_Cal
   ('a'..'f').each do |n|
     define_method "press_#{n}" do
       case @mode
-      when OperationType::DEC
-        @number = @number.to_i * 10 + n
       when OperationType::HEX
-        @number = @number.to_i * 16 + n
-      when OperationType::BIN
-        @number = @number.to_i * 2 + n
+        @number = @number.to_i * 16 + n.from_hex
       end
     end
 
   end
 
 
-
-  def press_cleara
+  def press_clear
     @number = 0
   end
 
   {'add' => '+', 'sub' => '-', 'times' => '*', 'div' => '/'}.each do |meth, op|
     define_method "press_#{meth}" do
-      if @op
+     if @op
         press_equals
       end
       @op = op
@@ -96,29 +111,51 @@ class Smart_Cal
   end
 
   def press_bin
-    @mode = OperationType::Bin
+    @mode = OperationType::BIN
   end
 
   def press_equals
-    @number = @previous.send(@op, @number.to_i)
+    puts @number.class
+    @number = @previous.send(@op, @number)
     @op = nil
   end
 
 end
 
 number_field = nil
+mode_field = nil
 number = Smart_Cal.new
-Shoes.app :height => 300, :width => 200, :resizable => false do
+
+list = Array.new
+32.times do |i|
+   list.insert(-1, "bit#{i}")
+end
+
+Shoes.app :height => 300, :width => 600, :resizable => false do
   background "#EEC".."#996", :curve => 5, :margin => 2
 
   stack :margin => 2 do
 
-    stack :margin => 8 do
-      number_field = para strong(number)
+    flow :width => 540, :margin => 4 do
+      flow :width => 0.1 do
+      mode_field = para strong(number.mode)
+      end
+      flow :width => 0.9 do
+        number_field = para strong(number.to_s)
+      end
+
     end
 
-    flow :width => 218, :margin => 4 do
-      %w(7 8 9 / 4 5 6 * 1 2 3 - 0 Clr = + / a b c d e f).each do |btn|
+    
+    flow :width => 540, :margin => 4 do
+      list.map! do |name|
+        flow :width => 0.125 do @c = check; para name end
+        [@c, name]
+      end
+    end
+
+    flow :width => 540, :margin => 4 do
+      %w(7 8 9 / 4 5 6 * 1 2 3 - 0 Clr = + a b c d e f hex dec bin).each do |btn|
         button btn, :width => 46, :height => 46 do
           method = case btn
             when /[0-9]/; 'press_'+btn
@@ -132,7 +169,8 @@ Shoes.app :height => 300, :width => 200, :resizable => false do
           end
           
           number.send(method)
-          number_field.replace strong(number)
+          number_field.replace strong(number.to_s)
+          mode_field.replace strong(number.mode)
         end
       end
     end
